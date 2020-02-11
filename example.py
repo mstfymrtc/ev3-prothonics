cc8e78a94e28427bf5e82326ad0c9e2716c60c28
Mustafa Yumurtacı <mstfymrtci@gmail.com>
2020-02-11 02:34:14 -0800

    Add new behaviour file

--- example.py
+++ example.py
@@ -1,7 +1,33 @@
 import prothonics
 import numpy as np
 #eat=just increase the score
+#vrep robot sensor takılacak yer , hocaya sor
+#robotun dönüş haraketlerini ayarla 
+#herbir kare boyutunu optimal olarak ayarla
 
+
+#if frontColorSensor==yellow:
+#    move_forward()
+#    eat()
+#elif rightColorSensor==yellow:
+#    turn_right()
+#    move_forward()
+#    eat()
+#elif leftColorSensor==yellow:
+#    turn_left()
+#    move_forward()
+#    eat()
+#elif frontColorSensor==white:
+#    move_forward()
+#elif rightColorSensor==white:
+#    turn_right()
+#    move_forward()
+#elif leftColorSensor==white:
+#    turn_left()
+#    move_forward()
+#else: #frontColorSensor==red and rightColorSensor==red and leftColorSensor==red
+#    turn_backward()
+#------------------------------------------------------------------------------
 #if frontColorSensor==white:
     #move_forward()
 #elif frontColorSensor==yellow:
@@ -24,6 +50,7 @@ import numpy as np
             #move_forward()
             #eat()
         #else: öne,sağa ve sola gidemez
+
 
 
 
@@ -56,9 +83,9 @@ def eat():
     print("eat")
 
 
-color_sensor_front = 4 #ColorSensor(INPUT_3)
-color_sensor_right = 5
-color_sensor_left = 6
+color_sensor_front = 6 #ColorSensor(INPUT_3)
+color_sensor_right = 6
+color_sensor_left = 4
 
 colors = ["Unknown", "Black", "Blue", "Green", "Yellow", "Red", "White",
           "Brown"]
---
+++ old_behaviour.pl
@@ -0,0 +1,65 @@
+hasValue('FrontColorSensor', S1) :- perception([S1, _, _]).
+hasValue('RightColorSensor', S2) :- perception([_, S2, _]).
+hasValue('LeftColorSensor', S3) :- perception([_, _, S3]).
+%TODO:white yerine yellow öncelikli olacak
+%TODO:red,red,red durumu handle edilecek.
+
+
+%+++++++++++ front beyazsa ilerle
+takeDecision('MoveForward') :-
+    hasValue('FrontColorSensor', 'White'), !.
+%if true dont look for the remainings = cut operator
+
+%prolog decisionunu liste olarak alıp her item için iterate etmek
+%gerekebilir. örn decisionlar MoveForward ve Eat ise, sırasıyla
+%çalıştırılırlar
+
+%+++++++++++ front sarıysa ilerle sonra yemek ye
+takeDecision('MoveForward') :-
+    hasValue('FrontColorSensor', 'Yellow').
+
+takeDecision('Eat') :-
+    hasValue('FrontColorSensor', 'Yellow'), !.
+%+++++++++++ front kırmızı ve sağ beyazsa sağa dön sonra ilerle
+takeDecision('TurnRight') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'White').
+
+takeDecision('MoveForward') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'White'), !.
+
+%+++++++++++ front kırmızı ve sağ sarıysa sağa dön sonra ilerle, sonra yemek ye
+takeDecision('TurnRight') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Yellow').
+
+takeDecision('MoveForward') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Yellow').
+
+takeDecision('Eat') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Yellow'), !.
+%+++++++++++ front kırmızı ve sağ kırmızıysa ve sol beyazsa sola dön sonra ilerle
+
+takeDecision('TurnLeft') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Red'), hasValue('LeftColorSensor', 'White').
+
+takeDecision('MoveForward') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Red'), hasValue('LeftColorSensor', 'White'), !.
+
+%+++++++++++ front kırmızı ve sağ kırmızıysa ve sol sarıysa sola dön sonra ilerle sonra yemek ye
+
+takeDecision('TurnLeft') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Red'), hasValue('LeftColorSensor', 'Yellow').
+
+takeDecision('MoveForward') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Red'), hasValue('LeftColorSensor', 'Yellow').
+
+takeDecision('Eat') :-
+    hasValue('FrontColorSensor', 'Red'), hasValue('RightColorSensor', 'Red'), hasValue('LeftColorSensor', 'Yellow'), !.
+
+%+++++++++++ front kırmızı ve sağ kırmızı ve sol kırmızıysa
+
+
+
+
+
+
+
